a9e3396c9a5a37b9606788856b7edaf4dfe48a70
Florian Fröhlich <44257732+ffsws@users.noreply.github.com>
2021-02-04 12:32:24 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@
 export KUBECONFIG=/tmp/.kube/config
 oc login --token=`cat /var/run/secrets/kubernetes.io/serviceaccount/token` https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT --insecure-skip-tls-verify
 oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' > /tmp/csrs
+oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}'
 LINECOUNT=`wc -l /tmp/csrs | awk '{print $1}'`
 if [ $LINECOUNT -gt 0 ]; then
     cat /tmp/csrs | xargs oc adm certificate approve
